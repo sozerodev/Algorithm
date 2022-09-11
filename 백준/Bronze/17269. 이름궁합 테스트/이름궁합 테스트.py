c9e458f0ce07c692dec05fadee588dfd2919b720
Soyoung Kim <sozerodev@gmail.com>
2022-09-11 19:11:12 +0900

    [Bronze I] Title: 이름궁합 테스트, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/17269. 이름궁합 테스트/이름궁합 테스트.py
+++ 백준/Bronze/17269. 이름궁합 테스트/이름궁합 테스트.py
@@ -1,77 +1,32 @@
-import copy
-
-name_cnt = {
-    'A': 3,
-    'B': 2,
-    'C': 1,
-    'D': 2,
-    'E': 4,
-    'F': 3,
-    'G': 1,
-    'H': 3,
-    'I': 1,
-    'J': 1,
-    'K': 3,
-    'L': 1,
-    'M': 3,
-    'N': 2,
-    'O': 1,
-    'P': 2,
-    'Q': 2,
-    'R': 2,
-    'S': 1,
-    'T': 2,
-    'U': 1,
-    'V': 1,
-    'W': 1,
-    'X': 2,
-    'Y': 2,
-    'Z': 1
-}
+# 지문에 주어진 변수가 있으면 똑같이 따라서 주는 편이 좋다.
+N, M = map(int, input().split())
+A, B = input().split()
 
 
-
-name_len1, name_len2 = map(int, input().split())
-
-name = list(map(str, input().split()))
-name_sort = []
-
-for i in range(min(name_len1, name_len2)):
-    name_sort.append(name[0][i])
-    name_sort.append(name[1][i])
+# 코테는 의외로 이렇게 하나하나 적어줘야 하는 경우가 있다고 함
+alp = [3, 2, 1, 2, 4, 3, 1, 3, 1, 1, 3, 1, 3, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
 
 
+# 1. 문자열을 처리한다. 
+AB = ''
+min_len = min(N, M)
 
-if name_len1 > name_len2:
-    longer_name = name[0]
-else:
-    longer_name = name[1]
+for i in range(min_len):
+    AB += A[i] + B[i]
 
-for i in range(min(name_len1, name_len2), max(name_len1, name_len2)):
-    name_sort.append(longer_name[i])
+# 리스트[N:] 을 하면 N이 리스트의 길이보다 길어도
+# index out of range에러가 뜨지 않고 빈 문자열을 반환한다.
+AB += A[min_len:] + B[min_len:]
 
+# 문자열을 하나씩 반복하면서..
+# ord() : ASKII값 반환 
+# alp에 해당하는 알파벳의 인덱스 값을 반환하게 될 것
+lst = [alp[ord(i)- ord('A')] for i in AB]
 
-name_num = []
-for i in name_sort:
-    name_num.append(name_cnt[i])
+for i in range(N+M-2):
+    for j in range(N+M-1-i):
+        lst[j] += lst[j+1]
+        
 
+print("{}%".format(lst[0]%10*10 + lst[1] % 10))
 
-name_temp = copy.deepcopy(name_num)
-
-while (len(name_temp) >2):
-    temp = []
-    for i in range(len(name_temp) - 1):
-        name_sum = name_temp[i] + name_temp[i+1]
-        if name_sum > 9:
-            temp.append(int(str(name_sum)[-1]))
-            
-        else:
-            temp.append(name_sum)
-            
-    name_temp = temp
-
-
-if (name_temp[0] == 0):
-    print(f"{name_temp[1]}%")
-else:
-    print(f"{name_temp[0]}{name_temp[1]}%")
