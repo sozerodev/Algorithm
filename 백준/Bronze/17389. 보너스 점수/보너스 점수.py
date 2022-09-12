f5446665ce18b4c2b7da3b4e93ad141d019d9a9c
sozerodev <sozerodev@gmail.com>
2022-09-12 19:11:58 +0900

    [Bronze II] Title: 보너스 점수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/17389. 보너스 점수/보너스 점수.py
+++ 백준/Bronze/17389. 보너스 점수/보너스 점수.py
@@ -1,20 +1,33 @@
-N = input()
-S = input()
+N, S = input(), input()
+
+# 풀이 1
+# result = 0
+# default_point = 0
+
+# for num in range(len(S)):
+#     if S[num] == 'O':
+#         result += num+1+default_point
+#         default_point += 1
+
+#     else:
+#         default_point = 0
+
+# print(result)        
 
 
-result = 0
-default_point = 0
+# 풀이 2
+score, bonus = 0, 0
 
-for num in range(len(S)):
-
-    if S[num] == 'O':
-        result += num+1+default_point
-
-        default_point += 1
-
+for idx, OX in enumerate(S):
+    # enumerate를 쓰면 idx와 value를 함께 얻을 수 있다. 
+    if OX == "O":
+        # score += idx + 1 + bonus
+        # bonus += 1
+        score, bonus = score + idx + 1 + bonus, bonus + 1
+    
     else:
-        default_point = 0
+        bonus = 0  
 
+print(score)
 
-print(result)        
 
