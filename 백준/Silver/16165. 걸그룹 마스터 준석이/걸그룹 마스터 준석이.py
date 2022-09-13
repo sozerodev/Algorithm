061c27811cb70f94efcdf5d22384b7036d6a3e26
Soyoung Kim <sozerodev@gmail.com>
2022-09-14 01:08:15 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.py
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.py
@@ -2,25 +2,18 @@ N, M = map(int, input().split())
 
 idol_info = {}
 for team in range(N):
-    team_name = input()
-
-    member_len = int(input())
+    team_name, member_len = input(), int(input())
+    
     idol_info[team_name] = []
     for i in range(member_len):
         idol_info[team_name].append(input())
 
-    idol_info[team_name] = sorted(idol_info[team_name])
-
 for q in range(M):
-    q_answer = input()
-    q_type = int(input())
-
-
+    q_answer, q_type = input(), int(input())
     for key, value in idol_info.items():
         if q_answer in value or q_answer == key:
-            if q_type == 0:
-                for member in value:
-                    print(member)
-            else:
+            if q_type:
                 print(key)
-
+            else:
+                for member in sorted(value):
+                    print(member)
