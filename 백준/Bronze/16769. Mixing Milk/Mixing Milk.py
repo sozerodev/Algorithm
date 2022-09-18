3cb18f47a2b0c666cdce29ed29c64b74df260139
Soyoung Kim <sozerodev@gmail.com>
2022-09-18 15:15:08 +0900

    [Bronze II] Title: Mixing Milk, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/16769. Mixing Milk/Mixing Milk.py
+++ 백준/Bronze/16769. Mixing Milk/Mixing Milk.py
@@ -1,21 +1,18 @@
 def solution():
     
-    c_arr, m_arr = [], []
-    for _ in range(3):
-        c, m = map(int, input().split())
-
-        c_arr.append(c)
-        m_arr.append(m)
+    c_arr, m_arr = [0 for _ in range(3)], [0 for _ in range(3)]
+    for i in range(3):
+        c_arr[i], m_arr[i] = map(int, input().split())
 
     
     for i in range(100):
-        idx = i % 3
-        nxt = (i+1)%3
+        idx, nxt = i % 3, (i+1)%3
         
-        m_arr[idx], m_arr[nxt] = max(m_arr[idx] - (c_arr[nxt] - m_arr[nxt]), 0), min(c_arr[nxt], m_arr[idx]+m_arr[nxt])
+        m_arr[idx], m_arr[nxt] = max(m_arr[idx] - (c_arr[nxt] - m_arr[nxt]), 0),min(c_arr[nxt], m_arr[idx]+m_arr[nxt])
 
     for i in m_arr:
         print(i)
 
-if __name__ == '__main__':
+
+if __name__ == '__main__': 
     solution()
