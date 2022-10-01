56d80d04da9acf741ef0be898172c213298835ff
sozerodev <sozerodev@gmail.com>
2022-10-01 13:56:09 +0900

    template 수정

--- mySettings/example/solution.py
+++ mySettings/example/solution.py
@@ -1,5 +1,11 @@
 # launch.json은 커서가 존재하는 파일을 기준으로 실행됩니다.
 # 다음과 같이 예시를 넣으면 mySettings/input.txt 의 input값을 읽어
 # output.txt에서 출력이 됩니다.
-N = input()
-print(N)
+from sys import stdin
+input = stdin.readline
+
+def solution():
+
+
+if __name__ == '__main__': 
+    solution()
