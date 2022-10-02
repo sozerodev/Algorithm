301c0ad4a5d7bce67233e7f706a3bdf532cf7bb7
sozerodev <sozerodev@gmail.com>
2022-10-02 17:57:48 +0900

    update template

--- mySettings/example/solution.py
+++ mySettings/example/solution.py
@@ -1,8 +1,12 @@
 # launch.json은 커서가 존재하는 파일을 기준으로 실행됩니다.
 # 다음과 같이 예시를 넣으면 mySettings/input.txt 의 input값을 읽어
 # output.txt에서 출력이 됩니다.
+import sys
 from sys import stdin
 input = stdin.readline
+
+# bfs 를 쓸 때 필요함
+# sys.setrecursionlimit(10000)
 
 def solution():
 
