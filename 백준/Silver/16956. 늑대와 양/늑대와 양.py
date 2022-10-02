5eace5993bc88e94b53256456887ae2244b22694
sozerodev <sozerodev@gmail.com>
2022-10-02 17:51:43 +0900

    update boj 16956

--- 백준/Silver/16956. 늑대와 양/늑대와 양.py
+++ 백준/Silver/16956. 늑대와 양/늑대와 양.py
@@ -4,7 +4,7 @@ sys.setrecursionlimit(10000)
 input = stdin.readline
 
 
-def solution():
+def solution1():
     R, C = map(int, input().split())
     dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 
@@ -33,7 +33,38 @@ def solution():
             print("".join(i))
 
 
+def solution2():
+    R, C = map(int, input().split())
+    dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+
+    farm = [list(map(str, input().strip())) for _ in range(R)]
+
+    doom = False
+
+    for row in range(R):
+        for col in range(C):
+            if farm[row][col] == "W":
+                for w in range(4):
+                    new_row, new_col = row + dx[w], col + dy[w]
+                    if new_row < 0 or new_row == R or new_col < 0 or new_col == C:
+                        continue
+                    if farm[new_row][new_col] == "S":
+                        doom = True
+
+    if doom:
+        print(0)
+    else:
+        print(1)
+        for i in range(R):
+            for j in range(C):
+                if farm[i][j] not in 'SW':
+                    farm[i][j] = 'D'
+        
+        for i in farm:
+            print(''.join(i))
+            
 
 if __name__ == '__main__': 
-    solution()
+    # solution1()
+    solution2()
         
