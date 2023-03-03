a6914c084e689838eab1b9e299ca126c00256cee
Soyoung Kim <sozerodev@gmail.com>
2023-03-03 14:33:20 +0900

    [Bronze V] Title: A+B - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,12 @@
+from sys import stdin
+input = stdin.readline
+
+def solution():
+    T = int(input())
+    for i in range(T):
+        A, B = map(int, input().split())
+        print(f"Case #{i+1}: {A} + {B} = {A+B}")
+        
+if __name__ == '__main__': 
+    solution()
+
