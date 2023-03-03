c36d0d7465965fe2bd8468d5e474f3e1f7177973
Soyoung Kim <sozerodev@gmail.com>
2023-03-03 15:22:03 +0900

    다른 풀이 추가

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,11 +1,15 @@
 input = open(0).readline
 def solution():
     N = int(input())
-    sum = 0
-    for i in input().strip():
-        sum += int(i)
-       
-    print(sum) 
+    
+    # sum = 0
+    # for i in input().strip():
+    #     sum += int(i)
+    # print(sum) 
+    
+    # 이게 더 좋은듯
+    print(sum([int(i) for i in input().strip()]))
+        
+
 if __name__ == '__main__': 
     solution()
-
