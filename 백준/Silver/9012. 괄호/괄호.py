de90e4c4c5d9ee663ed6c8442fab05fadf8a9a8d
Soyoung Kim <sozerodev@gmail.com>
2023-03-15 12:36:51 +0900

    boj 9012 풀이 업데이트

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -2,6 +2,9 @@ input = open(0).readline
 
 if __name__ == '__main__':
     
+    # 풀이1
+    # open된 괄호의 수와 close 괄호의 수를 더하고 빼면 될거라 생각했는데
+    # 좀 복잡한거같다 괜히
     for i in range(int(input())):
         input_vps = input().strip()
         
@@ -13,13 +16,19 @@ if __name__ == '__main__':
                 open_vps -= 1
         
             if open_vps < 0:
-                print("NO")
                 break
                 
-        if open_vps < 0:
-            continue
         if open_vps != 0:
             print("NO")
         else:
             print("YES")      
         
+        
+    # 풀이 2
+    # () 괄호를 그냥 없애기
+    # for _ in range(int(input())):
+    #     s = input().strip()
+    #     while '()' in s:
+    #         s = s.replace('()','')
+        
+    #     print('NO') if s else print('YES')
