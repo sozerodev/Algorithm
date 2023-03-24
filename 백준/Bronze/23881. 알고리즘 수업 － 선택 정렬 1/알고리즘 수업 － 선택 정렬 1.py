b66c6391838ec8540afc3bfaea01eeedfb5d4e1e
Soyoung Kim <sozerodev@gmail.com>
2023-03-24 15:49:07 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 288 ms, Memory: 115296 KB -BaekjoonHub

--- 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
@@ -1,17 +1,17 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
-
-
 if __name__ == '__main__':
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
     
     cnt = 0
     for i in range(N-1, 0, -1):
-        max_idx = A.index(max(A[:i+1]))
+        max_idx = i
         
-        if i != max_idx:       
+        for j in range(i-1, -1, -1):
+            if A[max_idx] < A[j]:
+                max_idx = j
+        if i != max_idx:    
             A[i], A[max_idx] = A[max_idx], A[i]
             cnt += 1
         if cnt == K:
