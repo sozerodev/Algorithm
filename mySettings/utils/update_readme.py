d1b675a82c305e61edf64fa70be3e70d96a3d0a0
Soyoung Kim <sozerodev@gmail.com>
2023-03-26 13:53:21 +0900

    add: github actions workflow status badge

--- mySettings/utils/update_readme.py
+++ mySettings/utils/update_readme.py
@@ -92,6 +92,7 @@ def make_read_me(files_info):
 
     return f"""
 
+[![Python application](https://github.com/sozerodev/Algorithm/actions/workflows/python-app.yml/badge.svg)](https://github.com/sozerodev/Algorithm/actions/workflows/python-app.yml)
 # PS (Problem Solving) 키우기
 
 
