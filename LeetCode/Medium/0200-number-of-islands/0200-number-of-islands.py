8179a7486a4521c3f64786459bd3dcfc0dd25a6c
Soyoung Kim <sozerodev@gmail.com>
2023-03-28 18:47:00 +0900

    Time: 330 ms (42.35%), Space: 16.2 MB (97.06%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,31 @@
+from collections import deque
 class Solution:
     def __init__(self):
         self.result = 0
         self.dx, self.dy = [1, 0, -1, 0], [0, -1, 0, 1]
         self.visited = []
 
-    def numIslands(self, grid: List[List[str]]) -> int:
-        self.visited =  [[False]* len(grid[0]) for _ in range(len(grid))]
-    
-        cnt = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if not self.visited[row][col] and grid[row][col] == "1":
-                    self.dfs(grid, row, col)
-                    cnt += 1
-        return cnt
-                                
 
+                                
+    def bfs(self, grid, row, col):
+        global visited
+        self.visited[row][col] = True
+        
+        queue = deque()
+        queue.append((row, col))
+        while queue:
+            cur_x, cur_y = queue.popleft()
+            
+            for i in range(4):
+                new_row, new_col = cur_x + self.dx[i], cur_y + self.dy[i]
+                
+                if 0<= new_row <len(grid) and 0<= new_col < len(grid[0]) and not self.visited[new_row][new_col] and grid[new_row][new_col] == "1":
+                    self.visited[new_row][new_col] = True
+                    queue.append((new_row, new_col))
+            
+        
+
+        
     def dfs(self, grid, row, col):
         global visited
         self.visited[row][col] = True
@@ -26,3 +36,16 @@ class Solution:
             if 0<= new_row <len(grid) and 0<= new_col < len(grid[0]) and not self.visited[new_row][new_col] and grid[new_row][new_col] == "1":
                 self.dfs(grid, new_row, new_col)
         
+        
+        
+    def numIslands(self, grid) -> int:
+        self.visited =  [[False]* len(grid[0]) for _ in range(len(grid))]
+    
+        cnt = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if not self.visited[row][col] and grid[row][col] == "1":
+                    # self.dfs(grid, row, col)
+                    self.bfs(grid, row, col)
+                    cnt += 1
+        return cnt
