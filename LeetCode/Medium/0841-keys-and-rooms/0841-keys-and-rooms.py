6f34ac149552f8c4907a0a30a150256f9eb4157a
Soyoung Kim <sozerodev@gmail.com>
2023-03-29 17:31:07 +0900

    Time: 67 ms (67.50%), Space: 14.3 MB (81.33%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.py
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,7 +1,24 @@
+from collections import deque
+
 class Solution:
     def __init__(self) -> None:
         self.visited = []
 
+    def bfs(self, start_v, rooms):
+        self.visited[start_v] = True
+        
+        queue = deque()
+        queue.append(start_v)
+        
+        while queue:
+            cur_v = queue.popleft()
+            
+            for v in rooms[cur_v]:
+                if not self.visited[v]:
+                    self.visited[v] = True
+                    queue.append(v)
+        
+        
     def dfs(self, cur_v, rooms):
         self.visited[cur_v] = True
         
@@ -15,9 +32,14 @@ class Solution:
         self.visited[0] = True
         
         for v in rooms[0]:
-            self.dfs(v, rooms)
-
-        if self.visited.count(False) != 0:
-            return False
-        else:
+            #self.dfs(v, rooms)
+            self.bfs(v, rooms)
+            
+            
+        if self.visited.count(False) == 0:
             return True
+        else:
+            return False
+        
+        
+        
