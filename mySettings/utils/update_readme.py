730d74046b18eb6b71db8c963a79fd881bc6b8e1
Soyoung Kim <sozerodev@gmail.com>
2023-04-08 19:07:18 +0900

    chore: 공백 라인 추가

--- mySettings/utils/update_readme.py
+++ mySettings/utils/update_readme.py
@@ -95,6 +95,7 @@ def make_read_me(files_info):
 # PS (Problem Solving) 키우기
 
 [![LeetCode sozerodev](https://img.shields.io/badge/dynamic/json?style=flat&labelColor=black&color=%23ffa116&label=Solved&query=solvedOverTotal&url=https%3A%2F%2Fleetcode-badge.vercel.app%2Fapi%2Fusers%2Fsozerodev&logo=leetcode&logoColor=yellow)](https://leetcode.com/sozerodev/)
+
 [![Solved.ac 프로필](http://mazassumnida.wtf/api/generate_badge?boj=sozerodev)](https://solved.ac/sozerodev)
 
 
